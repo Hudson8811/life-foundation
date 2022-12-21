51bad577816268d17cc3f25d7b10e1eea3854c7a
ArtZ <snickgem@gmail.com>
2022-12-22 03:34:57 +0400

    fix

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -6,9 +6,33 @@ ScrollMagicPluginGsap(ScrollMagic, TweenMax, TimelineMax);
 'use strict';
 
 $(document).ready(function () {
+	/* Negative margin for scroll sections */
+	const setNegativeOffset = (section, side) => {
+		if (window.matchMedia('(min-width: 1636px)').matches) {
+			switch (side) {
+				case 'left':
+					section.css('width', 'calc(100% + ' + (($(window).width() - section.closest('.container').outerWidth()) / 2 + 20) + 'px)');
+					section.css('margin-left', -($(window).width() - section.closest('.container').outerWidth()) / 2 - 20);
+					break;
+				case 'right':
+					section.css('width', 'calc(100% + ' + (($(window).width() - section.closest('.container').outerWidth()) / 2 - 20) + ')');
+					section.css('margin-right', -($(window).width() - section.closest('.container').outerWidth()) / 2 - 20);
+					break;
+			}
+		}
+	}
+
+	const setNegativeToSections = () => {
+		setNegativeOffset($('#pinContainer1'), 'left');
+		setNegativeOffset($('#pinContainer2'), 'right');
+		setNegativeOffset($('#pinContainer3'), 'left');
+	}
+
 	/* Scroll 1 */
 	var controller1 = new ScrollMagic.Controller();
-	var width1 = -($('#scrollTrigger1').get(0).scrollWidth - $('#pinContainer1').width()) + 'px';
+	var width1 = window.matchMedia('(max-width: 1635px)').matches ?
+		-($('#scrollTrigger1').get(0).scrollWidth - $('#pinContainer1').width()) + 'px' :
+		-($('#scrollTrigger1').get(0).scrollWidth - $('#pinContainer1').width()) + ($(window).width() - $('#pinContainer1').closest('.container').outerWidth()) / 2 + 20 + 'px';
 	var horzTl1 = new TimelineMax().to('#scrollTrigger1', 1, {x: width1}, 0.3);
 
 	var scene1 = new ScrollMagic.Scene({
@@ -22,7 +46,9 @@ $(document).ready(function () {
 
 	/* Scroll 2 */
 	var controller2 = new ScrollMagic.Controller();
-	var width2 = -($('#scrollTrigger2').get(0).scrollWidth - $('#pinContainer2').width()) + 'px';
+	var width2 = window.matchMedia('(max-width: 1635px)').matches ?
+		-($('#scrollTrigger2').get(0).scrollWidth - $('#pinContainer2').width()) + 'px' :
+		-($('#scrollTrigger2').get(0).scrollWidth - $('#pinContainer2').width()) + ($(window).width() - $('#pinContainer2').closest('.container').outerWidth()) / 2 + 20 + 'px';
 	var horzTl2 = new TimelineMax().to('#scrollTrigger2', 1, {x: width2}, 0.3);
 
 	var scene2 = new ScrollMagic.Scene({
@@ -50,7 +76,9 @@ $(document).ready(function () {
 
 	/* Scroll 3 */
 	var controller3 = new ScrollMagic.Controller();
-	var width3 = -($('#scrollTrigger3').get(0).scrollWidth - $('#pinContainer3').width()) + 'px';
+	var width3 = window.matchMedia('(max-width: 1635px)').matches ?
+		-($('#scrollTrigger3').get(0).scrollWidth - $('#pinContainer3').width()) + 'px' :
+		-($('#scrollTrigger3').get(0).scrollWidth - $('#pinContainer3').width()) + ($(window).width() - $('#pinContainer3').closest('.container').outerWidth()) / 2 + 20 + 'px';
 	var horzTl3 = new TimelineMax().to('#scrollTrigger3', 1, {x: width3}, 0.3)
 
 	var scene3 = new ScrollMagic.Scene({
@@ -62,9 +90,12 @@ $(document).ready(function () {
 		.setTween(horzTl3)
 		.addTo(controller3);
 
-	/*window.addEventListener('resize', () => {
-		window.location.reload();
-	});*/
+	setNegativeToSections();
+
+	window.addEventListener('resize', () => {
+		setNegativeToSections();
+		//window.location.reload();
+	});
 
 	/* Anchor */
 	document.querySelectorAll('.anchor').forEach(anchor => {
